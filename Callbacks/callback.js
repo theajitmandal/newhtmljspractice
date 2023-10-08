2c250f715097aa1fa331d95d42dc37f4d498a97c
ajitdhanukmandal <theajitmandal@gmail.com>
2023-10-08 20:58:27 +0545

    this is next commit

--- Callbacks/callback.js
+++ Callbacks/callback.js
@@ -1,11 +1,22 @@
-function myDisplayer(sum){
-    document.getElementById("demo").innerHTML = sum;
-}
+// function myDisplayer(sum){
+//     document.getElementById("demo").innerHTML = sum;
+// }
 
-function myCalculator(a, b){
-    let c = a + b;
-    return c;
+// function myCalculator(a, b){
+//     let c = a + b;
+//     return c;
+// }
+// let result = myCalculator(2, 4);
+// myDisplayer(result);
+
+
+setInterval(myFunction, 1000);
+
+function myFunction() {
+  let d = new Date();
+  document.getElementById("demo").innerHTML=
+  d.getHours() + ":" +
+  d.getMinutes() + ":" +
+  d.getSeconds();
 }
-let result = myCalculator(2, 4);
-myDisplayer(result);
 
